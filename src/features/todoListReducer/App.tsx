aaacd7e23a12ed51e43a2b3b3af0dae6d13facd9
Bertollo-Nicolas <118696143+Bertollo-Nicolas@users.noreply.github.com>
2024-09-04 11:02:17 +0200

    Update App.tsx

--- src/features/todoListReducer/App.tsx
+++ src/features/todoListReducer/App.tsx
@@ -7,6 +7,7 @@ import FakeBrowserHader from '@/components/exercices/ui/FakeBrowserHeader';
 import DashboardLayout from './components/layout/Dashboard-layout';
 import TodoList from './components/dashboard/TodoList';
 import AppProvider from './provider';
+import CategoryList from './components/dashboard/CategoryList';
 
 export const theme = 'useReducer()';
 
@@ -18,9 +19,12 @@ const App: React.FC = () => {
                 <Header exercice={useReducerExercice1} />
                 <div className="content-exercice pt-8 roboto-medium w-5/6 mx-auto mb-10 drop-shadow-md">
                     <FakeBrowserHader />
-                    <div className="app-todo relative bg-gray-100 p-5 text-neutral-950" style={{height: "600px"}}>
-                        <DashboardLayout />
-                        <TodoList />
+                    <div className="app-todo relative bg-gray-100 p-5 text-neutral-950 flex" style={{height: "700px"}}>
+                        <CategoryList />
+                        <div className='w-full ml-12'>
+                            <DashboardLayout />
+                            <TodoList />
+                        </div>
                     </div>
                 </div>
             </AppProvider>
