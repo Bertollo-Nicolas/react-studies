8473d5488907a8efc355423c24ea3765ea28f8c3
Bertollo-Nicolas <118696143+Bertollo-Nicolas@users.noreply.github.com>
2024-09-04 23:24:01 +0200

    delete unused dependencies

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import exercises from '../exercises';
 import './home.scss'
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
 
 const Home: React.FC = () => {
   const [exercisesWithThemes, setExercisesWithThemes] = useState<{ name: string; path: string; theme: string }[]>([]);
@@ -31,26 +23,13 @@ const Home: React.FC = () => {
   return (
     <div className='content bg-zinc-950 text-zinc-100'>
       <h3 className="font-semibold tracking-tight group flex items-center gap-2 text-lg ">Bienvenue ! Choisissez un exercice :</h3>
-      <Table className="">
-        <TableHeader>
-            <TableRow>
-                <TableHead>Exercices</TableHead>
-                <TableHead>Thème</TableHead>
-            </TableRow>
-        </TableHeader >
-        <TableBody>
+
           {exercisesWithThemes.map((exercise, index) => (
-              <TableRow key={index}>
-                  <TableCell>
-                      <Link to={`/${exercise.path}`}>{exercise.name}</Link>
-                  </TableCell>
-                  <TableCell>
-                    {exercise.theme}
-                  </TableCell>
-              </TableRow>
+
+              <Link key={index}to={`/${exercise.path}`}>{exercise.name}</Link>
+
           ))}
-        </TableBody>
-      </Table>
+
       <nav>
         <ul>
           
