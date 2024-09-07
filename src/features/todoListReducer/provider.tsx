15ce4bcb865f331d97ef897be13b6206e00fe5b7
Bertollo-Nicolas <118696143+Bertollo-Nicolas@users.noreply.github.com>
2024-09-07 20:26:57 +0200

    add CategoryProvider

--- src/features/todoListReducer/provider.tsx
+++ src/features/todoListReducer/provider.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import TodosProvider from './features/todos/stores/contexts/TodosContext';
+import CategoryProvider from './features/categories/stores/contexts/CategoriesContext';
 
 
 const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <TodosProvider>
-            {children}
+            <CategoryProvider>
+                {children}
+            </CategoryProvider>
         </TodosProvider>
     );
 };
