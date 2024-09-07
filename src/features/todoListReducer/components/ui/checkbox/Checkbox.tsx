fd9b2a0d5ee183395237eb89ee0649d5376c2453
Bertollo-Nicolas <118696143+Bertollo-Nicolas@users.noreply.github.com>
2024-09-07 20:24:27 +0200

    add read only on input

--- src/features/todoListReducer/components/ui/checkbox/Checkbox.tsx
+++ src/features/todoListReducer/components/ui/checkbox/Checkbox.tsx
@@ -11,7 +11,7 @@ const Checkbox: React.FC<CheckboxProps> = ({ isChecked }) => {
                 ? "checkbox w-6 h-6 rounded-md border-neutral-200 border-2 cursor-pointer transition-all duration-500 ease-in-out"
                 : "checkbox w-6 h-6 rounded-md border-neutral-200 border-2 cursor-pointer bg-black transition-all duration-500 ease-in-out"
         }>
-            <input type="checkbox" name="completed" checked={isChecked} className='hidden' />
+            <input type="checkbox" name="completed" checked={isChecked} className='hidden' readOnly />
         </div>
     );
 };
