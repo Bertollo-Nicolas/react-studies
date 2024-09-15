1c3c47a923b9a9a1be17c158d1fd0ff9b72a55d4
Bertollo-Nicolas <118696143+Bertollo-Nicolas@users.noreply.github.com>
2024-09-15 23:00:48 +0200

    Add new logo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,11 +1,15 @@
 import { features } from '@/features';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo-react-studies.svg'
 
 export const Sidebar: React.FC = () => {
   return (
     <aside className="w-72 h-screen bg-slate-900 text-slate-400 p-4 sticky top-0 border border-slate-800">
-      <h2 className="text-xl font-bold mb-4">React Studies</h2>
+      <div className="flex items-center">
+        <img src={logo} alt="logo-react-studies" className='w-10 mr-4' />
+        <h2 className="text-2xl text-neutral-100 font-bold">React Studies</h2>
+      </div>
       <nav>
         <ul>
           <li>
